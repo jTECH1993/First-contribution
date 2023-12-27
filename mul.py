a53a4ca6e275bfc73f436839aad41f0f0190858b
Muhammad Talha Jahangir <59771511+jTECH1993@users.noreply.github.com>
2023-12-28 01:18:24 +0500

    Third commit

---
+++ mul.py
@@ -0,0 +1,11 @@
+# This program will calculate the multiplication of two numbers
+
+# Getting the two numbers for multiplication
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculating the multiplication
+result = num1 * num2
+
+# Displaying the result
+print("The multiplication of the two numbers is: ", result)
--- sum.py
+++ sum.py
@@ -2,5 +2,6 @@ def sum_numbers(a, b):
     return a + b
 # Now add comment and
 print("Some Minor Change")
+print("Again add another changing")
 result = sum_numbers(5, 10)
 print(result)
